7729e675943dce41f6947647cebf8d4a24483ac8
Wachira Beatrice <BeatriceWambui>
2019-09-23 17:15:26 +0300

    create registrationform and login form in the cforms.html

---
+++ app/auth/views.py
@@ -0,0 +1,34 @@
+from flask import render_template,redirect,url_for
+from . import auth
+from ..models import User
+from .forms import RegistrationForm,LoginForm
+from .. import db
+from flask_login import login_user,login_required,logout_user,login_user
+
+@auth.route('/create_account',methods = ["GET","POST"])
+def create_account():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(email = form.email.data, username = form.username.data,password = form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        return redirect(url_for('auth.login'))
+    return render_template('auth/register.html', form=form)
+
+@auth.route('/login',methods=['GET','POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(username = form.username.data).first()
+        if user is not None and user.verify_password(form.password.data):
+            login_user(user,form.remember.data)
+            return redirect(request.args.get('next') or url_for('main.index'))
+
+        flash('Invalid username or Password')
+    return render_template('auth/login.html',form = form)
+
+@auth.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for("main.index"))
